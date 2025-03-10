959112c7bf76f3c9a9104cd964c1b18a68deeace
oumaima <oumaimaghannem21@gmail.com>
2025-03-10 13:45:35 +0100

    ajout de dossier.py a branche main

--- dossier.py
+++ dossier.py
@@ -1,4 +1,5 @@
 
+
  data=[1,3,5]
 if data :
     print('lasomme est : ',sum(data))
@@ -6,4 +7,12 @@ if data :
     print('le max est',max(data))
 else :
     print('dossier vide')
+
+=======
+def somme (T)
+s=0
+fot t in T :
+s+=t
+return s 
+
 
